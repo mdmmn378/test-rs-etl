9ba7b67e2a8c97c7832f8ac8346a5bcc4c45ef0b
Mamun <mdmmn378@gmail.com>
2023-08-27 06:44:09 +0600

    Added http source

--- src/sources.rs
+++ src/sources.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-
+use reqwest;
 use serde_json;
 use tokio::sync::mpsc::Sender;
 use tokio::time::{sleep, Duration};
@@ -40,6 +40,54 @@ impl SourceTrait for Source {
         }
     }
 }
+
+pub struct HttpSource {
+    name: String,
+    tx: Sender<serde_json::Value>,
+    url: String,
+}
+
+impl HttpSource {
+    pub fn new(name: String, tx: Sender<serde_json::Value>, url: String) -> HttpSource {
+        HttpSource { name, tx, url }
+    }
+    async fn send_request(&self) -> serde_json::Value {
+        let body = reqwest::get(&self.url).await.unwrap().text().await.unwrap();
+        // convert body to json
+        let json_body: serde_json::Value = serde_json::from_str(&body).unwrap();
+        json_body
+    }
+}
+
+#[async_trait]
+impl SourceTrait for HttpSource {
+    async fn run(&self) {
+        println!("{}: run", self.name);
+        loop {
+            let res = self.send_request().await;
+            let err = self.tx.send(res).await;
+            match err {
+                Ok(_) => (),
+                Err(e) => println!("Error: {}", e),
+            };
+            sleep(Duration::from_secs(1)).await;
+        }
+    }
+}
+#[tokio::test]
+async fn test_http_source() {
+    use tokio::sync::mpsc::channel;
+    use tokio::sync::mpsc::Receiver;
+
+    let (tx, _): (Sender<serde_json::Value>, Receiver<serde_json::Value>) = channel(100);
+    let source = HttpSource::new(
+        "source".to_string(),
+        tx,
+        "http://localhost:8000/rust/events".to_string(),
+    );
+    source.send_request().await;
+}
+
 #[tokio::test]
 async fn test_source() {
     use crate::sinks::Sink;
---
+++ src_py/app.py
@@ -0,0 +1,29 @@
+import random
+import string
+from datetime import datetime
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+def generate_random_string(ln: int = 10):
+    return "".join(random.choice(string.ascii_letters) for _ in range(ln))
+
+
+def generated_pseudo_event(source_name: str):
+    return {
+        "source": source_name,
+        "name": generate_random_string(),
+        "timestamp": str(datetime.now().isoformat()),
+    }
+
+
+@app.get("/{source}/events")
+async def events(source: str):
+    return [generated_pseudo_event(source) for _ in range(10)]
